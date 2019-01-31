fcc332b4806ac8b963da95cb29f7de85ee103382
Alex <secretarius@ukr.net>
2019-01-31 17:09:26 +0000

    Make some function

---
+++ js/index.js
@@ -0,0 +1,30 @@
+//let greatings="Hello!";
+//alert(greatings);
+
+//let age = +prompt("Enter your age");
+//let years = age + 1;
+//alert(years);
+
+//let age = parseInt(prompt("Enter your age"));
+//let years = (age + 1);
+//alert(years);
+let age = parseInt(prompt("Please enter your age: "));
+
+//function add(a, b, c){
+    //const res = a + b + c;
+    //return res;
+//}
+
+//const total = add(1, 2, 3);
+//alert(total);
+
+function checkAge(age) {
+    if(age <= 18) {
+        return "child";
+        
+    } else { 
+        return "adult"
+    }
+}
+
+alert("You are " + checkAge(age));
